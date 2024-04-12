b8d3d4635c6b0bb0e73ba2284d976551d4110736
Brian Hannan <bhannan@blueorigin.com>
2024-04-11 17:09:55 -0700

    Add test runner script

---
+++ vol-3-sort-and-search/ch-5-internal-sorting/alg-c-comparison-counting/runtests.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+make
+./test_comp_count
+
+exit_code=$?
+
+if [[ exit_code -eq 0 ]]; then
+    echo "Test suite passed."
+else
+    echo "Test suite failed with exit code $exit_code."
+fi
