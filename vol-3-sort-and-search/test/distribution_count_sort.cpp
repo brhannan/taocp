85a9ad7eaefc86f0b0ab67b13788dd1842a46e6a
Brian Hannan <bhannan@blueorigin.com>
2024-10-07 14:25:30 -0700

    clean up test

--- vol-3-sort-and-search/test/distribution_count_sort.cpp
+++ vol-3-sort-and-search/test/distribution_count_sort.cpp
@@ -4,11 +4,13 @@ extern "C" {
     #include "distribution_count_sort.h"
 }
 
+#define ARRAY_LENGTH(arr) (sizeof(arr) / sizeof((arr)[0]))
+
 TEST(distribution_count_tests, single_elem) {
     unsigned int K[] = {1};
     unsigned int S[] = {0};
     unsigned int S_exp[] = {1};
-    unsigned int len = 1;
+    unsigned int len = ARRAY_LENGTH(K);
     distribution_count_sort(K, S, len);
     ASSERT_EQ(S[0], S_exp[0]);
 }
@@ -17,7 +19,7 @@ TEST(distribution_count_tests, two_elems) {
     unsigned int K[] = {1, 0};
     unsigned int S[] = {0, 0};
     unsigned int S_exp[] = {0, 1};
-    unsigned int len = 2;
+    unsigned int len = ARRAY_LENGTH(K);
     distribution_count_sort(K, S, len);
     for(unsigned int i = 0; i<len; i++) {
         ASSERT_EQ(S[i], S_exp[i]);
@@ -28,7 +30,7 @@ TEST(distribution_count_tests, five_elems) {
     unsigned int K[] = {3, 2, 5, 4, 6};
     unsigned int S[] = {0, 0, 0, 0, 0};
     unsigned int S_exp[] = {2, 3, 4, 5, 6};
-    unsigned int len = 5;
+    unsigned int len = ARRAY_LENGTH(K);
     distribution_count_sort(K, S, len);
     for(unsigned int i = 0; i<len; i++) {
         ASSERT_EQ(S[i], S_exp[i]);
